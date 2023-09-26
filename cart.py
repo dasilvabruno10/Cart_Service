83233bcdec58c6efac8cf16d115a977156cb060c
dasilvabruno10 <124288685+dasilvabruno10@users.noreply.github.com>
2023-09-25 22:49:40 -0400

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,31 +1,25 @@
-# cart_service.py
+
 
 from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
 
-PRODUCT_SERVICE_URL = 'https://product-service-ppqh.onrender.com'  # Replace with your Product Service URL
+PRODUCT_SERVICE_URL = 'https://product-service-ppqh.onrender.com' 
 
 @app.route('/cart/<int:user_id>', methods=['GET'])
 def get_cart(user_id):
     # Simulate fetching the user's cart from the Cart Service
-    # This should make requests to the Product Service to retrieve product information
-    # and calculate the total price for each item in the cart
-    # Return the cart information as JSON
     return jsonify({'user_id': user_id, 'cart': []})
 
 @app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
 def add_to_cart(user_id, product_id):
     # Simulate adding a product to the user's cart
-    # This should make requests to the Product Service to check product availability
-    # and update the user's cart accordingly
     return jsonify({'message': f'Product {product_id} added to cart'})
 
 @app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
 def remove_from_cart(user_id, product_id):
     # Simulate removing a product from the user's cart
-    # This should make requests to the Product Service to update the user's cart
     return jsonify({'message': f'Product {product_id} removed from cart'})
 
 if __name__ == '__main__':
